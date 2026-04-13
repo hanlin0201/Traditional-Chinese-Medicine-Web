6d168930c56eb89e7594fbce222cf5341a1a9ea6
Atlantic793 <2452060@tongji.edu.cn>
2026-04-13 16:36:40 +0800

    首页节气 Bento 头部（四时之序、副标题、日期）横向居中

--- src/views/MenuIndexView.vue
+++ src/views/MenuIndexView.vue
@@ -1014,7 +1014,11 @@ onUnmounted(() => {
   width: 100%;
 }
 .seasonal-mini-head--bento .seasonal-mini-titles {
+  display: flex;
+  flex-direction: column;
   align-items: center;
+  text-align: center;
+  width: 100%;
 }
 .seasonal-mini-main--bento {
   gap: 4px;
@@ -1178,7 +1182,7 @@ onUnmounted(() => {
   overflow: hidden;
   max-width: 100%;
 }
-.seasonal-mini-head {
+.seasonal-mini-head:not(.seasonal-mini-head--bento) {
   display: flex;
   align-items: flex-start;
   justify-content: space-between;
